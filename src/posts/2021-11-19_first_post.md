1bc35b23ccaca52db74748ca43deb6c0029a60aa
Fatjon Hushi <me@fatjoni.com>
2022-04-13 18:53:27 +0200

    todo work on slug page

--- src/posts/2021-11-19_first_post.md
+++ src/posts/2021-11-19_first_post.md
@@ -3,4 +3,26 @@ title: First Post
 date: 2021-11-19T10:50:03.906Z
 thumbnail: /uploads/testtttt.jpg
 ---
+
 This is the first post
+Here are some headings:
+
+## Here's an h2
+
+Lorem ipsum dolor sit amet
+
+### This is an h3
+
+Lorem ipsum dolor sit amet
+
+#### As you've probably guessed, this is an h4
+
+Lorem ipsum dolor sit amet
+
+##### This, of course, is an h5
+
+Lorem ipsum dolor sit amet
+
+###### We're deep in h6 territory now
+
+Lorem ipsum dolor sit amet
--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -1,19 +1,32 @@
 <script context="module">
+	import { browser, dev } from '$app/env'
+	// we don't need any JS on this page, though we'll load
+	// it in dev so that we get hot module replacement...
+	export const hydrate = dev
+	// ...but if the client-side router is already loaded
+	// (i.e. we came here from elsewhere in the app), use it
+	export const router = browser
+	// since there's no dynamic data here, we can prerender
+	// it so that it gets served as a static asset in prod
+	export const prerender = true
+
 	/** @type {import('./[slug]').Load} */
-	export function load({ params }) {
-		const post = import.meta.glob(`../../posts/${params.slug}.md`)
+	export async function load({ params }) {
+		const post = await import(`../../posts/${params.slug}.md`)
 		console.log(post)
 
 		return {
 			props: {
-				post: params.slug
+				postContent: post.default,
+				postTitle: post.metadata.title
 			}
 		}
 	}
 </script>
 
 <script>
-	export let post
+	export let postContent, postTitle
 </script>
 
-<h1>{post}</h1>
+<h1>{postTitle}</h1>
+<p>{postContent}</p>
