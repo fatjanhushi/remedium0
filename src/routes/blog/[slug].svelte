225cd67e36242abc4ceae13221af1aa843e6b27c
Fatjon Hushi <me@fatjoni.com>
2022-04-13 18:26:43 +0200

    work to be done on the slug page

---
+++ src/routes/blog/[slug].svelte
@@ -0,0 +1,19 @@
+<script context="module">
+	/** @type {import('./[slug]').Load} */
+	export function load({ params }) {
+		const post = import.meta.glob(`../../posts/${params.slug}.md`)
+		console.log(post)
+
+		return {
+			props: {
+				post: params.slug
+			}
+		}
+	}
+</script>
+
+<script>
+	export let post
+</script>
+
+<h1>{post}</h1>
